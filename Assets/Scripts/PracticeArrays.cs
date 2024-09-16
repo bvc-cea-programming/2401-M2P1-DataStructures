9025d6ab1aa479fd3fc546d3bca86f1581a0e1ae
flamypow <wytchen90s@gmail.com>
2024-09-16 16:41:49 -0600

    Arrays and List

--- Assets/Scripts/PracticeArrays.cs
+++ Assets/Scripts/PracticeArrays.cs
@@ -2,9 +2,13 @@ using UnityEngine;
 
 public class PracticeArrays : MonoBehaviour
 {
+    [SerializeField] private GameObject _skeletonInstance;
+    [SerializeField] private GameObject[] skeletonPrefabs;
+    private Vector3 spawnLocation;
+    private int randomNum;
     private void Start()
     {
-        
+        spawnLocation = _skeletonInstance.transform.localPosition;
     }
 
     private void Update()
@@ -17,7 +21,12 @@ public class PracticeArrays : MonoBehaviour
     
     private void GenerateRandomEnemy()
     {
-        
+        if (_skeletonInstance != null)
+        {
+            Destroy(_skeletonInstance);
+        }
+        randomNum = Random.Range(0, skeletonPrefabs.Length);
+        _skeletonInstance = Instantiate(skeletonPrefabs[randomNum], spawnLocation, Quaternion.identity);
     }
     
 }
--- Assets/Scripts/PracticeLists.cs
+++ Assets/Scripts/PracticeLists.cs
@@ -5,9 +5,17 @@ using UnityEngine.EventSystems;
 
 public class PracticeLists : MonoBehaviour
 {
+    List<GameObject> collectables = new List<GameObject>();
+    private GameObject _collectableInstance;
+
+    [SerializeField] private GameObject spawnLocationObject;
+
+    private int randomNum;
+    private Vector3 spawnLocation;
+
     private void Start()
     {
-        
+        spawnLocation = spawnLocationObject.transform.position;
     }
 
     private void Update()
@@ -20,11 +28,18 @@ public class PracticeLists : MonoBehaviour
 
     public void CollectObjects(GameObject obj)
     {
-        
+        obj.SetActive(false);
+        collectables.Add(obj);
     }
     
     private void GenerateRandomItem()
     {
-        
+        if (_collectableInstance != null)
+        {
+            Destroy(_collectableInstance);
+        }
+        randomNum = Random.Range(0, collectables.Count);
+        _collectableInstance = Instantiate(collectables[randomNum], new Vector3(spawnLocation.x, collectables[randomNum].transform.position.y, spawnLocation.z), Quaternion.identity);
+        _collectableInstance.SetActive(true);
     }
 }
