4694e44f90f3f3b7e48425bb5856ad4e1c3f655d
deepanshu <deepanshu120100@gmail.com>
2024-09-16 17:31:15 -0600

    Part2 done

--- Assets/Scripts/PracticeLists.cs
+++ Assets/Scripts/PracticeLists.cs
@@ -1,10 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
 public class PracticeLists : MonoBehaviour
 {
+    public Transform spawner;
+    public List<GameObject> ItemsCollected = new List<GameObject>();
+    public GameObject currentItemSpawned; 
+
     private void Start()
     {
         
@@ -20,11 +25,27 @@ public class PracticeLists : MonoBehaviour
 
     public void CollectObjects(GameObject obj)
     {
-        
+        ItemsCollected.Add(obj);
+        obj.SetActive(false);
+
     }
     
     private void GenerateRandomItem()
     {
-        
+        if (currentItemSpawned != null) // just checking
+        {
+
+            currentItemSpawned.SetActive(false);
+
+        }
+
+        int randomIndex = Random.Range(0, ItemsCollected.Count); // randomly pick items
+        GameObject randomItem = ItemsCollected[randomIndex];
+
+        randomItem.transform.position = spawner.position; // randomly move to spawner
+        randomItem.SetActive(true);
+
+        currentItemSpawned = randomItem;
+
     }
 }
