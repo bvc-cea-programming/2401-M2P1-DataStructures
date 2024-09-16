69742588e519ab2f7e38d397292918381d93ee1c
deepanshu <deepanshu120100@gmail.com>
2024-09-16 17:07:19 -0600

    part1 done

--- Assets/Scripts/PracticeArrays.cs
+++ Assets/Scripts/PracticeArrays.cs
@@ -2,6 +2,12 @@ using UnityEngine;
 
 public class PracticeArrays : MonoBehaviour
 {
+    [SerializeField]
+    public GameObject[] skeletonP;
+    [SerializeField]
+    public Transform spawner;
+    [SerializeField]
+    private GameObject cEnemy; //current enemy
     private void Start()
     {
         
@@ -11,13 +17,23 @@ public class PracticeArrays : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.Space))
         {
+            DestroyPreviousEnemy();
             GenerateRandomEnemy();
         }
     }
     
     private void GenerateRandomEnemy()
     {
-        
+        int randomIndex = Random.Range(0, skeletonP.Length);
+
+        cEnemy = Instantiate(skeletonP[randomIndex], spawner.position, spawner.rotation);
     }
-    
+    public void DestroyPreviousEnemy()
+    {
+        if (cEnemy != null)
+        {
+            Destroy(cEnemy);
+        }
+
+    }
 }
