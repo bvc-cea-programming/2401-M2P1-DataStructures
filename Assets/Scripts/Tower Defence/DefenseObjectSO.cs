2d326dba7c40632b8d3dfc41d18363f2abbe8d2f
sayed akid <samsayed07866@gmail.com>
2024-09-16 21:36:05 -0600

    inclass activity

--- Assets/Scripts/Tower Defence/DefenseObjectSO.cs
+++ Assets/Scripts/Tower Defence/DefenseObjectSO.cs
@@ -1,6 +1,12 @@
 using UnityEngine;
 
+[CreateAssetMenu(menuName = "DefenceObject", fileName = "NewDefenceObject")]
 public class DefenseObjectSO : ScriptableObject
 {
-    
+    public string objectName;
+    public string description;
+    public GameObject objectPrefab;
+    public Sprite objecticon;
+    public int price;
 }
+
