b28a59e2132f81d5e0d6cd091e185c85bd3af628
flamypow <wytchen90s@gmail.com>
2024-09-16 18:17:53 -0600

    finished Activity

--- Assets/Scripts/Tower Defence/DefenseObjectSO.cs
+++ Assets/Scripts/Tower Defence/DefenseObjectSO.cs
@@ -1,6 +1,12 @@
 using UnityEngine;
 
+[CreateAssetMenu(fileName = "Data", menuName = "ScriptableObjects/DefenseScriptableObject", order = 1)]
 public class DefenseObjectSO : ScriptableObject
 {
-    
+    [SerializeField] public string objectName;
+    [SerializeField] public string objectDescription;
+    [SerializeField] public int objectPrice;
+    [SerializeField] public float fireRate;
+    [SerializeField] public GridObject objectPrefab;
+    [SerializeField] public Sprite objectSprite;
 }
--- Assets/Scripts/Tower Defence/GridManager.cs
+++ Assets/Scripts/Tower Defence/GridManager.cs
@@ -3,16 +3,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Serialization;
+using static UnityEditor.Progress;
 
 public class GridManager : MonoBehaviour
 {
     [SerializeField] private int gridSize = 10;
     [SerializeField] private Vector2 gridOffset = new Vector2(0.5f, 0.5f);
-    [SerializeField] private GameObject baseGridPrefab;
-    
+    [SerializeField] private GridObject baseGridPrefab;
+    [SerializeField] private GameObject gameBoard;
     [SerializeField] private List<DefenseObjectSO> defenseObjects = new List<DefenseObjectSO>();
     
     private GridObject[,] _grid;
+    private GridObject[,] _gridObjects;
+
+    private int randomNum;
 
     private void Start()
     {
@@ -21,7 +25,16 @@ public class GridManager : MonoBehaviour
 
     private void GenerateGrid()
     {
-        
+        //GridObject a = baseGridPrefab;
+        _grid = new GridObject[gridSize, gridSize];
+        _gridObjects = new GridObject[gridSize, gridSize];
+        for (int x = 0; x < gridSize; x++)
+        {
+            for (int y = 0; y < gridSize; y++)
+            {
+                _grid[x, y] = Instantiate<GridObject>(baseGridPrefab, new Vector3(gridOffset.x + x, 0, gridOffset.y + y), Quaternion.identity, gameBoard.transform);
+            }
+        }
     }
 
     public GridObject GetGridObject(int x, int y)
@@ -36,6 +49,17 @@ public class GridManager : MonoBehaviour
 
     public void SetGridObject(int x, int y)
     {
-        
+        if (x >= 0 && x < gridSize && y >= 0 && y < gridSize)
+        { 
+            randomNum = UnityEngine.Random.Range(0, defenseObjects.Count);
+            if (_gridObjects[x, y] != null)
+            {
+                Destroy(_gridObjects[x, y].gameObject);
+                _gridObjects[x, y] = null;
+            }
+            _gridObjects[x, y] = Instantiate<GridObject>(defenseObjects[randomNum].objectPrefab, _grid[x, y].transform, false);
+            _gridObjects[x, y].transform.SetLocalPositionAndRotation(Vector3.zero, Quaternion.identity);
+        }
+
     }
 }
